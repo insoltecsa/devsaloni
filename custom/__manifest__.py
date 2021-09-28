62c2f76d187a548590b0870863e0a62e45ce74ff
Byron Estrada <bestrada1981@gmail.com>
2021-09-27 20:53:56 -0600

    add view

--- custom/__manifest__.py
+++ custom/__manifest__.py
@@ -19,13 +19,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sales'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/res_partner.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
