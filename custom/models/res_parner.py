2ca3f97bac07be745de3d0d563aaeea0eb02a101
Byron Estrada <bestrada1981@gmail.com>
2021-09-27 20:38:49 -0600

    string4

--- custom/models/res_parner.py
+++ custom/models/res_parner.py
@@ -18,7 +18,4 @@ class Partner(models.Model):
         ('10', 'Octubre'),
         ('11', 'Noviembre'),                                
         ('12', 'Diciembre')        
-    ])
-
-    position = fields.Selection([('before', 'Before'),
-                                ('after', 'After')], string='Position')
+    ], string='Mes cumpleaños')
