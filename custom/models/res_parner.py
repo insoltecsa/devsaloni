f001c0cf06643d604908844e395eadef561d6d2e
Byron Estrada <bestrada1981@gmail.com>
2021-09-27 20:34:59 -0600

    string2

--- custom/models/res_parner.py
+++ custom/models/res_parner.py
@@ -6,16 +6,16 @@ class Partner(models.Model):
 
     dia_cumpleanios =  fields.Integer()
     mes_cumpleanios = fields.Selection([
-        (1, 'Enero'),
-        (2, 'Febrero'),
-        (3, 'Marzo'),
-        (4, 'Abril'),
-        (5, 'Mayo'),
-        (6, 'Junio'),
-        (7, 'Julio'),
-        (8, 'Agosto'),
-        (9, 'Septiembre'),
-        (10, 'Octubre'),
-        (11, 'Noviembre'),                                
-        (12, 'Diciembre'),        
+        ('1', 'Enero'),
+        ('2', 'Febrero'),
+        ('3', 'Marzo'),
+        ('4', 'Abril'),
+        ('5', 'Mayo'),
+        ('6', 'Junio'),
+        ('7', 'Julio'),
+        ('8', 'Agosto'),
+        ('9', 'Septiembre'),
+        ('10', 'Octubre'),
+        ('11', 'Noviembre'),                                
+        ('12', 'Diciembre')        
     ])
